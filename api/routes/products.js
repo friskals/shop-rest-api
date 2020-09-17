606cfe35b29b6def2213930820af1a07fdfa3d05
Friska L. Sianturi <friskasianturi23@gmail.com>
2020-09-17 23:21:58 +0700

    feat: parsing body and handle CORS

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,8 +10,13 @@ router.get('/', (req, res, next) => {
 
 
 router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
     res.status(201).json({
-        message: 'Products were created'
+        message: 'Products were created',
+        createdProduct: product
     })
 })
 
